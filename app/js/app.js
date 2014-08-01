34bc49fe921a70cec4733f2e17caa33508328532
Amrutha Rao. Kasamalla <amruthrao4u@gmail.com>
2014-08-01 14:44:19 +0530

    created a new branch for development and done with hello world assignment

---
+++ app/js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module("tycoons-app", [])
+   .controller('helloWorldCtrl', function($scope){
+		$scope.name = "Hello World!";	
+});
